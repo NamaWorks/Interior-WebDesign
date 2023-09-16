5fbceec1425f65b33a8ac8c870fc661124507b5c
NamaWorks <pedronayamartinez@hotmail.com>
2023-09-16 13:47:54 +0200

    add CSS continuation and start with JS

---
+++ project/main.js
@@ -0,0 +1,78 @@
+const products = [
+    //Accesories for the room
+    {
+        name: 'Charlotte Set',
+        price: 30,
+        category: 'Accessories for the room',
+        image: './assets/img/product-img/acc-for-the-room/pexels-charlotte-may-5825576.jpg/',
+    },
+    {
+        name: 'Karolina Set',
+        price: 15,
+        category: 'Accessories for the room',
+        image: './assets/img/product-img/acc-for-the-room/pexels-karolina-grabowska-4207892.jpg',
+    },
+    {
+        name: 'Rachel Set',
+        price: 120,
+        category: 'Accessories for the room',
+        image: './assets/img/product-img/acc-for-the-room/pexels-rachel-claire-4825713.jpg',
+    },
+    {
+        name: 'Vlada Set',
+        price: 75,
+        category: 'Accessories for the room',
+        image: './assets/img/product-img/acc-for-the-room/pexels-rachel-claire-4825713.jpg',
+    },
+    // Kitchen items
+    {
+        name: 'Blank Set',
+        price: 275,
+        category: 'Kitchen items',
+        image: './assets/img/product-img/kitchem-items/pexels-blank-space-2647714.jpg',
+    },
+    {
+        name: 'Jonathan Set',
+        price: 1050,
+        category: 'Kitchen items',
+        image: './assets/img/product-img/kitchem-items/pexels-jonathan-borba-5570224.jpg',
+    },
+    {
+        name: 'Medhat Set',
+        price: 1320,
+        category: 'Kitchen items',
+        image: './assets/img/product-img/kitchem-items/pexels-medhat-ayad-447592.jpg',
+    },
+    {
+        name: 'Mister Set',
+        price: 1320,
+        category: 'Kitchen items',
+        image: './assets/img/product-img/kitchem-items/pexels-mister-mister-2442904.jpg',
+    },
+    // Minimal & modern chairs
+    {
+        name: 'Joaquin Set',
+        price: 120,
+        category: 'Minimal & modern chairs',
+        image: './assets/img/product-img/minimal-n-modern-chairs/pexels-pixabay-533157.jpg',
+    },
+    {
+        name: 'Diana Set',
+        price: 360,
+        category: 'Minimal & modern chairs',
+        image: './assets/img/product-img/minimal-n-modern-chairs/pexels-rachel-claire-5490336.jpg',
+    },
+    {
+        name: 'Valerie Set',
+        price: 830,
+        category: 'Minimal & modern chairs',
+        image: './assets/img/product-img/minimal-n-modern-chairs/pexels-valeria-boltneva-827518',
+    },
+    {
+        name: 'Vecislava Set',
+        price: 470,
+        category: 'Minimal & modern chairs',
+        image: './assets/img/product-img/minimal-n-modern-chairs/pexels-vecislavas-popa-1571463.jpg',
+    },
+    // Outdoor Decor
+]
